6c095d059b58b708a06b9cb21e98cd3704677586
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-01-02 14:36:35 +0100

    fix: rename env variabls

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -28,14 +28,15 @@ import { getFirestore } from "firebase/firestore";
     const url = `${apiBase}/tools?sortBy=collections`;
  */
 
+// * DEVELOPMENT CREDENTIALS
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_DEV_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_DEV_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_DEV_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_DEV_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_DEV_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_DEV_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_DEV_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // const isDevelopment = window.location.origin.includes("localhost");
