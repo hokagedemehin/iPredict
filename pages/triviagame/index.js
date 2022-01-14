70c03f9d129691c36c8ea95774c95461c74302ad
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-01-15 00:06:56 +0100

    fix: fixed unused vaiables and imports

--- pages/triviagame/index.js
+++ pages/triviagame/index.js
@@ -1,13 +1,13 @@
-import { Heading } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { useUser } from "../../utils/auth/userContext";
-import NavHeader from "../../components/nav/header.component";
-import Layout from "../../components/layout/layout";
-import TriviaHomePageComponent from "../../components/triviagame/trivia.homepage.component";
+import { Heading } from '@chakra-ui/react';
+// import { useRouter } from "next/router";
+import React, { useEffect } from 'react';
+import { useUser } from '../../utils/auth/userContext';
+import NavHeader from '../../components/nav/header.component';
+import Layout from '../../components/layout/layout';
+import TriviaHomePageComponent from '../../components/triviagame/trivia.homepage.component';
 
 const TriviaGamesPage = () => {
-  const router = useRouter();
+  // const router = useRouter();
   const { user } = useUser();
   // console.log(user);
   useEffect(() => {
@@ -16,10 +16,10 @@ const TriviaGamesPage = () => {
     // }
   }, [user]);
   return (
-    <Layout name="trivia" desc="I-Predict Trivia Game">
+    <Layout name='trivia' desc='I-Predict Trivia Game'>
       <NavHeader />
-      <div className="max-w-sm mx-auto">
-        <div className="text text-center my-5">
+      <div className='max-w-sm mx-auto'>
+        <div className='text text-center my-5'>
           <Heading>Trivia Game</Heading>
         </div>
         <TriviaHomePageComponent />
